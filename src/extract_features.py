0715575bc91d06ead78e6c652187845b79a8b3c2
Mosfirat Nasreen <mxn020@student.bham.ac.uk>
2026-01-04 13:55:48 +0000

    added resnet feature extractor

--- src/extract_features.py
+++ src/extract_features.py
@@ -13,7 +13,7 @@ batch_size = 32
 
 def get_device():
     if torch.backends.mps.is_available():
-        print("uising m1 GPU acceleration")
+        print("using m1 GPU acceleration")
         return torch.device("mps")
     elif torch.cuda.is_available():
         print("using nvidia gpu acceleration")
@@ -22,3 +22,19 @@ def get_device():
         print("no gpu detected > using cpu")
         return torch.device("cpu")
     
+device = get_device()
+
+#load resnet, remove classification head and return the 2048-dim feature extractor
+def get_resnet_feature_extractor():
+    try:
+        weights = models.ResNet50_Weights.IMAGENET1K_V1
+        model = models.resnet50(weights = weights)
+    except:
+        model = models.resnet50(pretrained=True) #fallback to older versions
+
+    modules = list(model.children())[:-1] #remove the last fc layer
+    model = nn.Sequential(*modules) # output = (batch, 2048, 1, 1)
+
+    model.to(device)
+    model.eval() #freeze layers
+    return model    
